c64317aaff35e243e484bed2a84b428e9d9cadfe
hwalabs <hwalabs@gmail.com>
2025-09-29 08:54:25 +0700

    feat: add logout functionality and session management across multiple pages

--- backend/querySql/proses_login.php
+++ backend/querySql/proses_login.php
@@ -1,14 +1,21 @@
 <?php
-    include __DIR__ . '/../database.php';
+// mengaktifkan session
+session_start();
+include __DIR__ . '/../database.php';
 
-    $email = $_POST['email'];
-    $pass = $_POST['password'];
-    $sql = "SELECT * FROM pengguna WHERE email='$email' && password='$pass' LIMIT 1";
-    $hasil = $conn->query($sql);
+$email = $_POST['email'];
+$pass = $_POST['password'];
+$sql = "SELECT * FROM pengguna WHERE email='$email' && password='$pass' LIMIT 1";
+$hasil = $conn->query($sql);
 
-    if ($hasil->num_rows > 0) {
-        header("Location: ../../sidebarMenu.php");
-    } else {
-        echo "Email atau Password Salah";
-    }
-?>
+if ($hasil->num_rows > 0) {
+    $row= $hasil->fetch_assoc();
+
+    $_SESSION['id_pengguna'] = $row['id_pengguna'];
+    $_SESSION['nama'] =$row['nama'];
+    $_SESSION['email'] =$row['email'];
+    
+    header("Location: ../../sidebarMenu.php");
+} else {
+    echo "Email atau Password Salah";
+}
